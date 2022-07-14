40e9381c8a8360ace4be6ef0947e6c5721af54c5
raphson1 <raphsoneric@gmail.com>
2022-07-14 13:45:19 -0500

    homeroutes

--- Main/controllers/home-routes.js
+++ Main/controllers/home-routes.js
@@ -21,6 +21,43 @@ router.get('/', async (req, res) => {
     } catch(err) {
         res.status(500).json(err)
     }
+});
+
+router.get('/blog/:id', async (req, res) => {
+    try {
+        const blogData = await Blog.findByPk(req.params.id, {
+            include: [
+                {
+                    model: User,
+                    attributes: ['name'],
+                }
+            ]
+        });
+        const blog = blogData.get({ plain: true });
+
+        res.render('blog', {
+            ...blog,
+            logged_in: req.session.logged_in
+        });
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+router.get('/profile', withAuth, async (req, res) => {
+    try {
+        const userData = await User.findByPk(req.session.user_id, {
+            attributes: { exclude: ['password'] },
+            include: [{ model: Blog }],
+        });
+
+        const user = userData.get({ plain: true });
+
+        res.render('profile', {
+            ...user,
+            logged_in: true
+        });
+    }
 })
 
 module.exports = router;
